0c6d3964a92bba41a5634755c05b880723c89e4b
Carlos HEnrique Maia Marques <carlosmarques89@outlook.com>
2018-08-12 05:05:33 -0300

    Testes aprimorados

--- my-app/src/test/java/com/tw/app/AppMainTest.java
+++ my-app/src/test/java/com/tw/app/AppMainTest.java
@@ -9,30 +9,20 @@ import org.junit.Test;
 import com.tw.app.util.FileUtil;
 
 public class AppMainTest {
+	
+	public static String NOME_ARQUIVO_SUCESSO = "/arquivo_de_input";
+	
+	public static String NOME_ARQUIVO_ESPERANDO_SUCESSO = "/arquivo_de_input_esperado";
+	
+	public static String NOME_ARQUIVO_VAZIO = "/arquivo_vazio";
+	
 	@Test
-    public void testConferenceTrackManagementMultipleFullDayEvents() throws IOException {
-    	testConferenceTrackManagement("/arquivo_de_input");
+    public void testeSucessoConferenciaInputEsperado() throws IOException {
+    	Conferencia conferenciaTeste = new ConferenciaAgenda()
+    			.agenda(FileUtil.getBufferedReaderForResourceFile(NOME_ARQUIVO_SUCESSO, this));
+    	
+    	assertTrue(FileUtil.contentEquals(NOME_ARQUIVO_ESPERANDO_SUCESSO, conferenciaTeste.toString(), this));
     }
 
-//    @Test
-//    public void testConferenceTrackManagementMultipleDayLessEvents() throws IOException {
-//    	testConferenceTrackManagement("/input_file_less_events");
-//    }
-//
-//    @Test
-//    public void testConferenceTrackManagementSingleDayEvents() throws IOException {
-//    	testConferenceTrackManagement("/input_file_single_day_events");
-//    }
-
-    private void testConferenceTrackManagement(String inputFile) throws IOException {
-    	Conferencia conference = new ConferenciaAgenda().agenda(
-    			FileUtil.getBufferedReaderForResourceFile(inputFile, this));
-    	assertTrue(FileUtil.contentEquals(getExpectedOutputFile(inputFile),
-    			conference.toString(), this));
-    }
-
-    private String getExpectedOutputFile(String inputFile) {
-    	return inputFile + "_esperado";
-    }
 
 }
